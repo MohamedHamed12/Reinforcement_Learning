a85201981ee04793d04def07521ea12a6212e629
MohamedHamed12 <mh1779371@gmail.com>
2023-12-15 07:15:49 +0200

    first_commit

---
+++ reinforcement/projectParams.py
@@ -0,0 +1,8 @@
+# projectParams.py
+# ----------------
+
+
+STUDENT_CODE_DEFAULT = 'analysis.py,qlearningAgents.py,valueIterationAgents.py'
+PROJECT_TEST_CLASSES = 'reinforcementTestClasses.py'
+PROJECT_NAME = 'Project 3: Reinforcement learning'
+BONUS_PIC = False
