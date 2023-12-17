f968955467e31a384d3d273a9d88b2cb0b48f64a
MohamedHamed12 <mh1779371@gmail.com>
2023-12-17 20:16:34 +0200

    Policies

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -7,44 +7,45 @@
 
 # Set the given parameters to obtain the specified policies through
 # value iteration.
-
+# python gridworld.py -g DiscountGrid -a value --discount 0.9 --noise 0 --livingReward -4 
+# python gridworld.py -g DiscountGrid -a value --discount 0.9 --noise 0 --livingReward -4 
 def question2():
     answerDiscount = 0.9
     answerNoise = 0
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = .1 
+    answerNoise = 0 
+    answerLivingReward = -4 
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.1 
+    answerNoise = 0.1
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = -1 # 0 ... -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 1
+    answerNoise = .1
+    answerLivingReward = -.5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = 100
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
